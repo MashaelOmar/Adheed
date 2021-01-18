85a0093fc654b6d27fcf67bec5f07586bd0ff106
Mashael <70836239+MashaelOmar@users.noreply.github.com>
2021-01-18 23:17:11 +0300

    Initial Commit

---
+++ Shared/AdheedApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdheedApp.swift
+//  Shared
+//
+//  Created by Mashael Alhabdan on 05/06/1442 AH.
+//
+
+import SwiftUI
+
+@main
+struct AdheedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Mashael Alhabdan on 05/06/1442 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
